f2387ec59b0fb38bc6165022ec95afbdd98cd605
Brendan <brendanmadden94@hotmail.com>
2025-10-20 13:07:32 +0100

    added variables for JS and function to get questions.

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+let countSpan = document.querySelector('.count span');
+let flagImgDiv = document.querySelector('.flag-img');
+let flagImg = document.querySelector('.flag-img img');
+let flagOptions = document.querySelector('.flag-options ul');
+let flagLis = document.querySelectorAll('.flag-options ul li');
+let score = document.querySelector('h3 span');
+let scoreDiv = document.querySelector('.score');
+let correctAns = document.querySelector('.score .right span');
+let incorrectAns = document.querySelector('.score .incorrect span');
+let btnNewGame = document.querySelector('#newGame');
+
+let currentIndex = 0;
+let rightAnswer = 0;
+
+function getQuestions() {
+  let myRequest = new XMLHttpRequest();
+  myRequest.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      let questions = JSON.parse(this.responseText);
+      let qCount = 10;
+      questionNum(qCount);
+      questions = questions.sort(() => Math.random() - Math.random()).slice(0, 10);
+      addQuestionData(questions[currentIndex], qCount);
