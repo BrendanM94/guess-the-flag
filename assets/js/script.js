a3e85b441e9c91d4e8e24715287c9049a7a1639f
BrendanM94 <71528810+BrendanM94@users.noreply.github.com>
2025-10-20 14:36:31 +0100

    Fix get question function (#14)
    * added variables for JS and function to get questions.
    
    * fix missing code for get question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,60 @@ let currentIndex = 0;
 let rightAnswer = 0;
 
 function getQuestions() {
+    let myRequest = new XMLHttpRequest();
+    myRequest.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let questions = JSON.parse(this.responseText);
+            //Number Of Question Each New Game
+            let qCount = 10;
+            questionNum(qCount);
+            //Random Question Each New Game
+            questions = questions.sort(() => Math.random() - Math.random()).slice(0, 10);
+
+            //Add Questions Data
+            addQuestionData(questions[currentIndex], qCount);
+
+            flagLis.forEach(li => {
+                li.addEventListener('click', () => {
+                    let rightAnswer = questions[currentIndex].right_answer;
+                    li.classList.add('active');
+                    //Increase Index 
+                    currentIndex++;
+
+                    //Check The Answer after 500ms
+                    setTimeout(() => {
+                        checkAnswer(rightAnswer, qCount);
+                    }, 500);
+
+                    setTimeout(() => {
+                        //Remove Previous Image Source
+                        flagImg.src = '';
+                        //Remove All Classes (active,success,wrong)
+                        li.classList.remove('active');
+                        li.classList.remove('success');
+                        li.classList.remove('wrong');
+
+                        //Add Questions Data To Show The Next Question
+                        addQuestionData(questions[currentIndex], qCount);
+                    }, 1000);
+
+                    //Show Results
+                    setTimeout(() => {
+                        showResults(qCount);
+                    }, 1002);
+                });
+            });
+        }
+    }
+    myRequest.open("GET", "js/flag_questions.json", true);
+    myRequest.send();
+}
+
+getQuestions();
+
+function questionNum(num) {
+    countSpan.innerHTML = num;
+}
   let myRequest = new XMLHttpRequest();
   myRequest.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
